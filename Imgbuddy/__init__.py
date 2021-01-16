5c46c1f1f8d41e79e0c317f182627fcf26a91231
joeyjoey1234 <37442571+joeyjoey1234@users.noreply.github.com>
2021-01-15 22:15:46 -0700

    Made check for Requests

--- Imgbuddy/__init__.py
+++ Imgbuddy/__init__.py
@@ -1,7 +1,16 @@
 import subprocess
 import sys
-subprocess.check_call([sys.executable, "-m", "pip", "install", 'requests'])
-from .meme import ImgGen
+import pkg_resources
+
+
+required = 'requests'
+installed = {pkg.key for pkg in pkg_resources.working_set}
+missing = required - installed
+
+if missing:
+    subprocess.check_call([sys.executable, "-m", "pip", "install", 'requests'])
+
+from .ImgGen import ImgGen
 
 
 
